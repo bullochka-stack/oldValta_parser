3ff0f93fd94d61ffb5aa704f2d20fddc992afd29
Vendetta-source <tamer.kab@mail.ru>
2021-02-20 18:06:54 +0300

    Доделан сбор ссылок

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from selenium import webdriver
 import time
 
-PATH = 'C:/Users/TAMER/PycharmProjects/pythonProject1/msedgedriver.exe'
+PATH = "C:/Users/Компот/Desktop/Python/oldValta parser/msedgedriver.exe"
 URL = 'http://old.valta.ru/catalog/'
 
 
@@ -14,18 +14,23 @@ time.sleep(3)
 driver.find_element_by_xpath("//*[@id='form-btn-region']/span").click()
 time.sleep(2)
 
-for i in range (0, 2, 1):
+for i in range(0, 242, 1):
     driver.execute_script("window.scrollTo(0, 100)")
     driver.find_element_by_class_name("button-text").click()
     time.sleep(3)
 
 products_links = driver.find_elements_by_class_name("p-lnk")
 
+
 for i, word in enumerate(products_links):
     products_links[i] = products_links[i].get_attribute("href")
-    if (products_links[i] == products_links[i-1]) & (i != 1):
-        products_links.remove(i)
-        continue
-    print(products_links[i])
-    print(len(products_links))
+
+y = products_links[::2]
+
+with open('url_list.txt', 'a') as file:
+    for line in y:
+        file.write(f'{line}\n')
+
+print(y)
+print(len(y))
 
